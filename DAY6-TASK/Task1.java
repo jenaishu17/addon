90f5add20c6f04b4744cacd0e3a4a6a711d1e012
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-12 11:34:48 +0530

    DAY6 TASK1 COMPLETED

---
+++ DAY6-TASK/Task1.java
@@ -0,0 +1,51 @@
+package addon_day6;
+
+public class Task1 {
+
+	int mystack[]=new int[5];
+	int top=-1;
+	
+	public void push(int val) {
+        if (top < mystack.length)
+        { 
+            mystack[++top] = val;            
+        } 
+        else
+        {
+            System.out.println("Stack Overflow!");
+        }
+    }
+	 public void pop() {
+	        if (top >= 0)
+	        { 
+	            System.out.println("Popped: " + mystack[top--]); 
+	        } 
+	        else
+	        {
+	            System.out.println("Stack Underflow!");
+	        }
+	    }
+	public void display() {
+        if (top >= 0) 
+        {
+            System.out.println("Stack Elements:");
+            for (int i = top; i >= 0; i--)
+            { 
+                System.out.println(mystack[i] + " ");
+            }
+        } 
+        else {
+            System.out.println("Stack is Empty!");
+        }
+    }
+	public static void main(String[] args) {
+		Task1 s=new Task1();
+		s.push(4);
+		s.push(3);
+		s.push(2);
+		s.push(1);
+		s.display();
+		s.pop();
+		s.display();
+	}
+}
